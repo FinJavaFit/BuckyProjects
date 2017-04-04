931269d7bd06a5de6a5d1be2cd758517b6fd2a87
Christopher Rawls <chris0_23@yahoo.com>
2017-04-03 19:09:07 -0500

    Add files via upload

---
+++ BuckySeries.java
@@ -0,0 +1,31 @@
+
+package buckyseries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BuckySeries {
+
+
+    public static void main(String[] args) {
+     
+        String[] things= {"eggs", "lasers", "hats", "pies"};
+        List<String> list1 = new ArrayList <String>();
+        
+        //add array items to list1
+        for(String x: things)
+            list1.add(x);
+        
+        String[] morethings = {"lasers", "hats"};
+        List<String> list2 = new ArrayList<String>(); 
+        
+        //add array items to list2
+        for(String y: morethings)
+            list2.add(y);
+        
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.printf("%s ", list1.get(i));
+        }
+    }
+}
