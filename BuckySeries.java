d34565eea223ca0ea8c0c68829ddca20813f206f
Christopher Rawls <chris0_23@yahoo.com>
2017-04-05 15:31:09 -0500

    Add files via upload

--- BuckySeries.java
+++ BuckySeries.java
@@ -1,47 +1,50 @@
 
 package buckyseries;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 
 public class BuckySeries {
 
 
     public static void main(String[] args) {
-     
-        String[] things= {"eggs", "lasers", "hats", "pies"};
-        List<String> list1 = new ArrayList <String>();
+    
+        String[] things = {"apples", "noobs", "pwnge","bacon","goATS"};
         
-        //add array items to list1
-        for(String x: things)
+        List<String> list1 = new LinkedList<String>();
+        
+        for(String x : things)
             list1.add(x);
         
-        String[] morethings = {"lasers", "hats"};
-        List<String> list2 = new ArrayList<String>(); 
-        
-        //add array items to list2
-        for(String y: morethings)
-            list2.add(y);
-        
-        //print out list1
-        for (int i = 0; i < list1.size(); i++) {
-            System.out.printf("%s ", list1.get(i));
+        String[] things2 = {"sausage", "bacon", "goats", "harrypotter"};
+        List<String> list2 = new LinkedList<String>();
+            for(String y : things2)
+                list2.add(y);
             
-          editlist(list1,list2);
-            System.out.println();
-             
-            for (int i = 0; i < list1.size(); i++) {
-            System.out.printf("%s ", list1.get(i));
-             }
-        }
-        
-            public static void editlist(Collection<String> L1, Collection<String>L2){
-        
-        Iterator<String> it = L1.iterator();
-        while(it.hasNext()) {
-            if (L2.contains(it.next())) {
-                it.remove();
-            }
+            list1.addAll(list2);
+            list2 = null;
+            
+            printMe(list1);
+            removeStuff(list1, 2, 5);
+            printMe(list1);
+            reverseMe(list1);
+    }
+    
+    //printMe method
+    private static void printMe(List<String> L) {
+        for(String b : L)
+            System.out.printf("%s ", b);
+        System.out.println();
+    }
+    
+    //removeStuff method
+    private static void removeStuff(List<String> L, int from, int to) {
+        L.subList(from, to).clear();
+    }
+    
+    //reverseMe method
+    private static void reverseMe(List<String> L) {
+        ListIterator<String> chris = L.listIterator(L.size());
+        while(chris.hasPrevious())
+            System.out.printf("%s ", chris.previous());
     }
 }
